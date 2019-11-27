fdc0c161458eb2686d0f2cb4d6a5e7ab5b8c5790
Ian <hi@ian.pe>
2019-11-27 22:45:51 +0000

    Add lang back and update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use PbbgIo\TitanFramework\Cronjobs;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +25,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        try {
+            $jobs = Cronjobs::where('enabled', true)->get();
+            foreach ($jobs as $job) {
+                $schedule->command($job->command)
+                    ->cron($job->cron);
+            }
+        } catch(\Exception $exception){}
     }
 
     /**
