f79b20049a72fe4a503a285073249cfb69da8da3
Kyle Ellis <ky.ellis83@gmail.com>
2020-01-04 09:06:30 -0800

    Added trait for banning this playable

--- app/User.php
+++ app/User.php
@@ -5,12 +5,14 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use KyleMassacre\BanUser\Entities\CanBanPlayable;
 use PbbgIo\Titan\Character;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable,
+        CanBanPlayable,
         HasRoles;
 
     /**
