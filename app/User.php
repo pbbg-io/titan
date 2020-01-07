a1e083c182777d5940ecdc059c0bd48eacbf543c
Kyle Ellis <ky.ellis83@gmail.com>
2020-01-07 03:54:46 -0800

    Removed trait from app/User and added the methods from CanBanPlayable

--- app/User.php
+++ app/User.php
@@ -5,14 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use KyleMassacre\BanUser\Entities\CanBanPlayable;
 use PbbgIo\Titan\Character;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable,
-        CanBanPlayable,
         HasRoles;
 
     /**
@@ -48,5 +46,53 @@ class User extends Authenticatable
 
     public function getCharacterAttribute() {
         return $this->characters->where('id', session('character_logged_in'))->first();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function banned()
+    {
+        return $this->morphMany(Ban::class, 'bannable');
+    }
+
+    /**
+     * Bans the playable class. If the ban exists, it will update the ban.
+     *
+     * @param string $reason
+     * @param null $time
+     * @param bool $forever
+     * @return bool
+     */
+    public function placeBan(string $reason, $time = null, bool $forever = false)
+    {
+        return Ban::updateOrCreate([
+            'bannable_id' => $this->attributes['id'],
+            'bannable_type' => get_class($this)
+        ], [
+            'reason' => $reason,
+            'ban_until' => ($time != null ? Carbon::parse($time)->toDateString() : null),
+            'forever' => $forever
+        ])->exists();
+    }
+
+    /**
+     * Checks to see if the if the playable is banned
+     *
+     * @return bool
+     */
+    public function isBanned()
+    {
+        return $this->banned()->exists();
+    }
+
+    /**
+     * Returns the value of the name column in the class
+     *
+     * @return mixed
+     */
+    public function getNameAttribute()
+    {
+        return $this->attributes['display_name'];
     }
 }
