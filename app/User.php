2103f81ef20edfca71dc4e09d4fb4a184a82caf2
Kyle Ellis <ky.ellis83@gmail.com>
2020-01-18 06:53:34 -0800

    Added methods for banning users

--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
+use PbbgIo\Titan\Ban;
 use PbbgIo\Titan\Character;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -52,5 +54,29 @@ class User extends Authenticatable
 
     public function character() {
         return $this->hasOne(Character::class, 'id', 'last_character_played');
+    }
+
+    public function banned()
+    {
+        return $this->morphMany(Ban::class, 'bannable');
+    }
+    public function placeBan(string $reason, $time = null, bool $forever = false)
+    {
+        return Ban::updateOrCreate([
+            'bannable_id' => $this->attributes['id'],
+            'bannable_type' => get_class($this)
+        ], [
+            'reason' => $reason,
+            'ban_until' => ($time != null ? Carbon::parse($time)->toDateString() : null),
+            'forever' => $forever
+        ])->exists();
+    }
+    public function isBanned()
+    {
+        return $this->banned()->exists();
+    }
+    public function getNameAttribute()
+    {
+        return $this->attributes['display_name'] ?? $this->attributes['name'];
     }
 }
